77ba9388442a403335801ad6e27a1cc8f2ff33df
DrewMurr2 <drewmurr2@gmail.com>
2020-07-08 09:14:42 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    salesForceCredentials: {
+        loginUrl: 'https://login.salesforce.com/',
+        userName: 'drewmurr2@gmail.com',
+        password: '@km2f7it@qNbMUU',
+        token: 'tDEN6dXJCZ2IdmHSfmESg0Eo'
+    },
+    testParameters: {
+        customObjectName: '',
+        picklistFieldName: '',
+        picklistValues: ['', '', '']
+    }
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = function addPicklistField(salesForceCredentials /**Object {} */ ,
+    customObjectName /**String */ ,
+    picklistFieldName /**String */ ,
+    picklistValues /**String Array */ ) {
+    return new Promise((resolve, reject) => {
+
+
+
+
+
+    })
+}
